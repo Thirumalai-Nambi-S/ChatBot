5b271c8f46d4cb597bc555a9a1c9bd7309ed3d07
THIRUMALAI NAMBI S <sthirumalainambi5802@gmail.com>
2026-07-17 13:56:22 +0530

    updated in app.py

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_origins=[
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://chatbot-v2.vercel.app"
+    "https://chatbot-v2-cyan.vercel.app"
     ],
     allow_credentials=False,
     allow_methods=["*"],
